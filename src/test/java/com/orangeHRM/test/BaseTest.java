fb83cfa39c6a987ced71a5f7b88d13aec93f17aa
Ananth0403 <anishananth2@gmail.com>
2025-05-14 07:17:33 +0530

    setup test done

---
+++ src/test/java/com/orangeHRM/test/BaseTest.java
@@ -0,0 +1,17 @@
+package com.orangeHRM.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import com.orangehrm.base.BaseClass;
+
+public class BaseTest extends BaseClass {
+
+	@Test
+	public void setupTest() {
+		// TODO Auto-generated method stub
+		String title = driver.getTitle();
+		Assert.assertEquals(title, "OrangeHRM");
+	}
+
+}
